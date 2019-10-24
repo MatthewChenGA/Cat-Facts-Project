f01b9f7423990a926e18dbf5699ed256154876c8
Matthew Chen <matthewchen8462@hotmail.com>
2019-10-24 13:35:11 -0400

    safety commit before adding anything else

--- cat-facts/src/components/Header.jsx
+++ cat-facts/src/components/Header.jsx
@@ -5,10 +5,11 @@ export default function Header(props) {
   return (
     <header>
       <div>
-        <p>Not GitHub<br></br>&nbsp;&nbsp;&nbsp;&nbsp;cat btw</p>
+        <p>Not GitHub<br></br>cat btw ^_^</p>
         <Link to="/">
           <img id="logo" src={props.logoSrc} alt="logo"></img>
         </Link>
+        <p>Home</p>
       </div>
       <h1>Meow Facts</h1>
     </header>
--- cat-facts/src/components/Main.jsx
+++ cat-facts/src/components/Main.jsx
@@ -8,10 +8,10 @@ export default function Main(props) {
       <p><span className="redText bold">Do you like cats too?</span></p>
       <p>Ah, no matters. You are here for some random cat trivia.Why not try and click some button?</p>
       <Link to="/catfact">
-        <button onClick={props.handleClick}>Get Cat Fact!</button>
+        <button className="main-button" onClick={props.handleClick}>Get Cat Fact!</button>
       </Link>
       <Link to="/randomfact">
-        <button onClick={props.handleRandomClick}>Get Random Animal Facts!</button>
+        <button onClick={props.handleRandomClick}>Get Random Animal Fact!</button>
       </Link>
     </div >
   )
